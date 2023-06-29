7aca421b3960b49719d22c3185b1f7b1397fca82
Eren Erturk <61586104+erenerturk1@users.noreply.github.com>
2023-06-29 08:57:44 +0300

    [URL]: Add macOS downloads

--- redirects.js
+++ redirects.js
@@ -1,3 +1,13 @@
 const redirects = {
 	example: 'https://example.com/',
+	sonoma: 'https://olarila.com/files/Torrents/Olarila%20Sonoma.raw.torrent',
+	ventura: 'https://olarila.com/files/Torrents/Olarila%20Ventura%2013.4.1.raw.torrent',
+	monterey: 'https://olarila.com/files/Torrents/Olarila%20Monterey%2012.6.7.raw.torrent',
+	bigsur: 'https://olarila.com/files/Torrents/Olarila%20BigSur%2011.7.8.raw.torrent',
+	catalina: 'https://www.olarila.com/files/Torrents/Olarila%20Catalina%2010.5.7.raw.torrent',
+	mojave: 'https://www.olarila.com/files/Torrents/Olarila%20Mojave%2010.14.6.raw.torrent',
+	highsierra: 'https://www.olarila.com/files/Torrents/Olarila%20High%20Sierra.raw.torrent',
+	sierra: 'https://www.olarila.com/files/Torrents/Olarila%20Sierra.raw.torrent',
+	elcapitan: 'https://www.olarila.com/files/Torrents/Olarila%20El%20Capitan.raw.torrent',
+	mavericks: 'https://www.olarila.com/files/Torrents/Olarila%20Mavericks.raw.torrent',
 };
